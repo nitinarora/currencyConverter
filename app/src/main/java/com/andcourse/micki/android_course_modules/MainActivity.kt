f9d51ad580637571bfed88575ce34100d105eb2f
nitin <nitinarora1981@gmail.com>
2019-02-10 05:48:12 +0100

    Added major functionality where rates are fetched from the internet, and also any from and to currencies are now supported by the backend

--- app/src/main/java/com/andcourse/micki/android_course_modules/MainActivity.kt
+++ app/src/main/java/com/andcourse/micki/android_course_modules/MainActivity.kt
@@ -4,7 +4,18 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.*
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +23,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private lateinit var currencyAmount: EditText
 
-    companion object {
-        const val CONVERSION_RATE_USD_TO_SEK:Float = 9.27f
+
+    private var currentConversionRate: Double = 0.0
+
+    private fun extractConversionRate(fromCurrency: String, toCurrency: String): Double {
+        val apiURL = URL("http://free.currencyconverterapi.com/api/v5/convert?q=$fromCurrency" + "_" + "$toCurrency&compact=y")
+
+        Volley.newRequestQueue(this).add<String?>(StringRequest(Request.Method.GET, apiURL.toString(), Response.Listener<String> { response ->
+            val conversionValue = JSONObject(response)
+            Log.i("Info", conversionValue.toString())
+            currentConversionRate = (conversionValue.get(conversionValue.names().get(0).toString()) as JSONObject)["val"] as Double
+        }, Response.ErrorListener { }))
+        return currentConversionRate
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,8 +48,8 @@ class MainActivity : AppCompatActivity() {
 
         convertButton.setOnClickListener {
             val amountEntered = currencyAmount.text ?: ""
-            val convertedAmountinSEK = if(!amountEntered.isEmpty()) {
-                amountEntered.toString().toFloat() * CONVERSION_RATE_USD_TO_SEK
+            val convertedAmountinSEK = if (!amountEntered.isEmpty()) {
+                amountEntered.toString().toFloat() * extractConversionRate("USD", "SEK")
             } else 0f
             val toast = Toast.makeText(this, "SEK: " + String.format("%.2f", convertedAmountinSEK), Toast.LENGTH_LONG)
             val toastView = toast.view
